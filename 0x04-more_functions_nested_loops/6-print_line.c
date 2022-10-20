1711b31dafe595e9b3f3b204c39e8e68f8ee0321
sagesalem7 <pastorsalem35@gmail.com>
2022-10-20 04:30:46 -0700

    task 6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,23 +1,17 @@
 #include "main.h"
 
 /**
- * print_diagonal - prints diagonal line n times.
- * @n: times diagonal line is printed.
+ * print_line - prints straight line n times.
+ * @n: times straight line is printed.
  * Return: no return.
  */
-void print_diagonal(int n)
+void print_line(int n)
 {
-	int i, j;
+	int i;
 
 	for (i = 0; i < n; i++)
 	{
-		for (j = 0; j < i; j++)
-		{
-			_putchar(' ');
-		}
-		_putchar(92);
-		if (i < (n - 1))
-			_putchar('\n');
+		_putchar(95);
 	}
 	_putchar('\n');
 }

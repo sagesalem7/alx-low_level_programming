00e115a09219b08088c66a39365e7d6996b7ff5c
Salem <pastorsalem35@gmail.com>
2023-09-07 14:36:45 -0700

    task 1

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main - Point of Entry
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}

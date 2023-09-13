d178f68d8048d658f5951dc858c67559f6c139a0
Salem <pastorsalem35@gmail.com>
2023-09-13 00:06:47 -0700

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints alphabets in lowercase
+ */
+
+void print_alphabet(void)
+{
+	char p;
+
+	for (p = 'a'; p <= 'z'; p++)
+	{
+		_putchar(p);
+	}
+	_putchar('\n');
+}
